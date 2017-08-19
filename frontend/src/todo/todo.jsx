6a95f153d07308e466a2028fe2cef37b7239dcf4
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-19 10:56:42 -0300

    Section 5, class 44

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div className='todo'>
+                <h1>Todo</h1>
+            </div>
+        );
+    }
+}
