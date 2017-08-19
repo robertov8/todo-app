d823419eaab52ad33b8664176e688947351220bf
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-19 18:17:02 -0300

    Section 5, class 60

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -19,6 +19,7 @@ export default class Todo extends Component {
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleClear = this.handleClear.bind(this);
 
         this.refresh();
     }
@@ -59,6 +60,10 @@ export default class Todo extends Component {
         this.refresh(this.state.description);
     }
 
+    handleClear() {
+        this.refresh();
+    }
+
     render() {
         return (
             <div className='todo'>
@@ -69,6 +74,7 @@ export default class Todo extends Component {
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd}
                     handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}
                 />
 
                 <TodoList
