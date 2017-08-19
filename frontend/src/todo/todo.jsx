112f0714d55d266bc3f79f3e2b4283ab340d7516
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-19 17:21:39 -0300

    Section 5, class 57

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -16,6 +16,8 @@ export default class Todo extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
         this.handleRemove = this.handleRemove.bind(this);
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
 
         this.refresh();
     }
@@ -40,6 +42,16 @@ export default class Todo extends Component {
             .then(resp => this.refresh());
     }
 
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => this.refresh());
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => this.refresh());
+    }
+
     render() {
         return (
             <div className='todo'>
@@ -53,6 +65,8 @@ export default class Todo extends Component {
 
                 <TodoList
                     list={this.state.list}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
                     handleRemove={this.handleRemove}
                 />
             </div>
