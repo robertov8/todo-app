48be641873f3c33d1b467bd22ac90d7585a0e19b
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-22 03:36:31 -0300

    Section 7, class 82

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import IconButton from '../template/iconButton';
-import { markAsDone, markAsPending } from './todoActions';
+import { markAsDone, markAsPending, remove } from './todoActions';
 
 const TodoList = props => {
     const renderRows = () => {
@@ -30,7 +30,7 @@ const TodoList = props => {
                         style='danger'
                         icon='trash-o'
                         hide={!todo.done}
-                        onClick={() => props.handleRemove(todo)}
+                        onClick={() => props.remove(todo)}
                     />
                 </td>
             </tr>
@@ -54,5 +54,5 @@ const TodoList = props => {
 }
 
 const mapStateToProps = state => ({list: state.todo.list});
-const mapDispatchToProps = dispatch => bindActionCreators({markAsDone, markAsPending}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({markAsDone, markAsPending, remove}, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
