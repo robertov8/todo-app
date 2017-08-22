eb4d645c8f1d8e80feeba7d10d8b935176853281
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-21 23:12:24 -0300

    Section 7, class 73

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -78,7 +78,6 @@ export default class Todo extends Component {
                 />
 
                 <TodoList
-                    list={this.state.list}
                     handleMarkAsDone={this.handleMarkAsDone}
                     handleMarkAsPending={this.handleMarkAsPending}
                     handleRemove={this.handleRemove}
